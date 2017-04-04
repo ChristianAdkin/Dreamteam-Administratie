8f0de148a757bd85b67a60d07da03d81aadefb51
ChristianAdkin <christianadkin@hotmail.com>
2017-04-04 10:16:31 +0200

    Initial API setup

---
+++ src/main/java/com/dreamteam/payd/administration/api/JAXRSConfig.java
@@ -0,0 +1,11 @@
+package com.dreamteam.payd.administration.api;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Created by Christian Adkin on 10/03/2017.
+ */
+@ApplicationPath("api")
+public class JAXRSConfig extends Application {
+}
---
+++ src/main/java/com/dreamteam/payd/administration/api/portal/PaymentResource.java
@@ -0,0 +1,10 @@
+package com.dreamteam.payd.administration.api.portal;
+
+import javax.ejb.Stateless;
+
+/**
+ * Created by Christian Adkin on 04/04/2017.
+ */
+@Stateless
+public class PaymentResource {
+}
