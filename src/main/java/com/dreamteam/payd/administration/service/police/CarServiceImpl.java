e0aeb232f5f19e58e14eff803aa2446d903e4c3e
ChristianAdkin <christianadkin@hotmail.com>
2017-04-04 10:19:07 +0200

    Added Service implementations

---
+++ src/main/java/com/dreamteam/payd/administration/service/police/CarServiceImpl.java
@@ -0,0 +1,10 @@
+package com.dreamteam.payd.administration.service.police;
+
+import javax.ejb.Stateless;
+
+/**
+ * Created by Christian Adkin on 04/04/2017.
+ */
+@Stateless
+public class CarServiceImpl {
+}
