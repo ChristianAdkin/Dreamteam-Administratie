68aec229c15af9a664f4e127511ba26c3bf97c79
ChristianAdkin <christianadkin@hotmail.com>
2017-05-16 13:11:56 +0200

    Changed portal resource

--- src/main/java/com/dreamteam/payd/administration/api/portal/UserPortalResource.java
+++ src/main/java/com/dreamteam/payd/administration/api/portal/UserPortalResource.java
@@ -1,5 +1,6 @@
 package com.dreamteam.payd.administration.api.portal;
 
+import com.dreamteam.payd.administration.service.portal.PaymentService;
 import com.dreamteam.payd.administration.service.portal.TranslocationService;
 
 import javax.ejb.Stateless;
@@ -9,8 +10,11 @@ import javax.inject.Inject;
  * Created by Christian Adkin on 04/04/2017.
  */
 @Stateless
-public class TranslocationResource {
+public class UserPortalResource {
 
     @Inject
+    private PaymentService paymentService;
+    @Inject
     private TranslocationService translocationService;
+
 }
