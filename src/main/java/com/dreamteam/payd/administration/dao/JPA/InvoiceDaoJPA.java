f26e775ebed4da9bce6099de3c536863efdb1c83
mickwonnink <mick.wonnink@live.nl>
2017-06-05 22:06:29 +0200

    Fix enum

--- src/main/java/com/dreamteam/payd/administration/dao/JPA/InvoiceDaoJPA.java
+++ src/main/java/com/dreamteam/payd/administration/dao/JPA/InvoiceDaoJPA.java
@@ -50,7 +50,10 @@ public class InvoiceDaoJPA extends BaseDaoJPA<Invoice> implements InvoiceDao {
 
     @Override
     public Long getAmountOpenInvoicesOfUser(Long userId) {
-        return entityManager.createQuery("SELECT COUNT(i) FROM Invoice i WHERE i.citizen.user.id = :userId AND i.invoiceStatus = com.dreamteam.payd.administration.model.InvoiceStatus.OPEN", Long.class).setParameter("userId", userId).getSingleResult();
+        return entityManager.createQuery("SELECT COUNT(i) FROM Invoice i WHERE i.citizen.user.id = :userId AND i.invoiceStatus = :status", Long.class)
+                .setParameter("userId", userId)
+                .setParameter("status", InvoiceStatus.OPEN)
+                .getSingleResult();
     }
 
     @Override
