93861685a1f491ad330a77f42a4e4d8dfe3eda1b
Stef <s.philipsen@student.fontys.nl>
2017-06-06 14:35:18 +0200

    RouteDao added

---
+++ src/main/java/com/dreamteam/payd/administration/dao/RouteDao.java
@@ -0,0 +1,13 @@
+package com.dreamteam.payd.administration.dao;
+
+import com.dreamteam.payd.administration.model.Car;
+import com.dreamteam.payd.administration.model.Route;
+
+import java.util.List;
+
+/**
+ * Created by Christian Adkin on 21/03/2017.
+ */
+public interface RouteDao extends BaseDao<Route> {
+
+}
